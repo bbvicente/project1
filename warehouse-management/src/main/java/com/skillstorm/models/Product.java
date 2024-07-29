26b21f3198313c0ff8cdd3ffc328a6944e01252c
Bruna B. Vicente <bvicente.bruna@gmail.com>
2024-07-29 13:52:03 -0400

    Add more details and Spring notations to the Warehouse and Product models

--- warehouse-management/src/main/java/com/skillstorm/models/Product.java
+++ warehouse-management/src/main/java/com/skillstorm/models/Product.java
@@ -1,20 +1,57 @@
 package com.skillstorm.models;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+@CrossOrigin(origins = "http://localhost:5173") //might change!!!
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int productId;
 
+    @Min(value = 4)
+    @Max(value = 16)
     private String sku;
 
+    @Column(length = 20)
     private String name;
 
+    @Min(value = 0)
+    @Max(value = 1000)
     private int quantity;
 
+    @Column(length = 7)
     private String storageLocation;
 
     //dateIn
     //dateOut
+
     //warehouse_id (FK)
+    @ManyToOne
+    @Cascade(CascadeType.PERSIST)
+    @NotNull
+    @JoinColumn(name = "warehouse_id")
+    @JsonIdentityReference(alwaysAsId = true)
+    private Warehouse warehouse;
+
     public Product() {
     }
 
@@ -66,7 +103,18 @@ public class Product {
         this.storageLocation = storageLocation;
     }
 
-    
+    public Warehouse getWarehouse_id() {
+        return warehouse;
+    }
 
-    
+    public void setWarehouse_id(Warehouse warehouse_id) {
+        this.warehouse = warehouse_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [productId=" + productId + ", sku=" + sku + ", name=" + name + ", quantity=" + quantity
+                + ", storageLocation=" + storageLocation + ", warehouse" + (warehouse == null ? null : warehouse.getName()) + "]";
+    }
+
 }
--- warehouse-management/src/main/java/com/skillstorm/models/Warehouse.java
+++ warehouse-management/src/main/java/com/skillstorm/models/Warehouse.java
@@ -1,17 +1,47 @@
 package com.skillstorm.models;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
+@Table(name = "warehouses")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Warehouse {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int warehouseId;
 
+    @Column(length = 10)
     private String name;
 
+    @Column(length = 20)
     private String city;
 
+    @Column(length = 2)
     private String state;
 
+    @Min(value = 0)
+    @Max(value = 1000)
     private int capacity;
 
+    @OneToMany(mappedBy = "product", targetEntity = Product.class)
+    @JsonBackReference
+    List<Product> products;
+    
     public Warehouse() {
     }
 
@@ -63,5 +93,10 @@ public class Warehouse {
         this.capacity = capacity;
     }
 
+    @Override
+    public String toString() {
+        return "Warehouse [warehouseId=" + warehouseId + ", name=" + name + ", city=" + city + ", state=" + state
+                + ", capacity=" + capacity + "]";
+    }
 
 }
