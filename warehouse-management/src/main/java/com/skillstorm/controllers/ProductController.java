6ebf160ea0b594b967be0dec1fabbeb73cd6b332
Bruna B. Vicente <bvicente.bruna@gmail.com>
2024-07-29 18:57:49 -0400

    Implementation of Product Controller, Service, Repository

--- warehouse-management/src/main/java/com/skillstorm/controllers/ProductController.java
+++ warehouse-management/src/main/java/com/skillstorm/controllers/ProductController.java
@@ -1,5 +1,71 @@
 package com.skillstorm.controllers;
 
-public class ProductController {
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillstorm.models.Product;
+import com.skillstorm.models.Warehouse;
+import com.skillstorm.services.ProductService;
+import com.skillstorm.services.WarehouseService;
+
+import jakarta.validation.Valid;
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService service;
+
+        public ProductController(ProductService service){
+            this.service = service;
+        }
+
+        @GetMapping
+        public Iterable<Product> findAllProducts() {
+            return service.findAllProducts();
+        }
+        
+        @GetMapping("/{id}")
+        public ResponseEntity<Product> findProductById(@PathVariable int id) {
+            Optional<Product> p = service.findProductById(id);
+
+            if (p.isPresent()) {
+                return ResponseEntity.ok(p.get());
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+
+        /*
+        TODO:
+            get all categories
+            get Product current inventory
+            get products in a Product
+        */
+        
+        @PostMapping
+        public void createProduct(@Valid @RequestBody Product p) {
+            // add some logic
+            service.saveProduct(p);
+        }
+        
+        @PutMapping("/{id}")
+        public void updateProduct(@PathVariable int id, @RequestBody Product p) {
+            service.updateProduct(id, p);
+        }
+        
+        @DeleteMapping("/{id}")
+        @ResponseStatus(code = HttpStatus.NO_CONTENT)
+        public void deleteProduct(@PathVariable int id) {
+            service.deleteProduct(id);
+        }
 }
--- warehouse-management/src/main/java/com/skillstorm/services/ProductService.java
+++ warehouse-management/src/main/java/com/skillstorm/services/ProductService.java
@@ -1,5 +1,53 @@
 package com.skillstorm.services;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import com.skillstorm.models.Product;
+import com.skillstorm.repositories.ProductRepository;
+
 public class ProductService {
 
+    private ProductRepository repo;
+    
+    public ProductService(ProductRepository repo){
+        this.repo = repo;
+    }
+    public Iterable<Product> findAllProducts() {
+        return repo.findAll();
+    }
+    
+    public Optional<Product> findProductById(int id) {
+        return repo.findById(id);
+    }
+    
+    public void saveProduct(Product p) {
+
+        if (repo.existsById(p.getProductId())) {
+            repo.updateProduct(p.getProductId(), p);
+            //Add an exception
+        }
+        repo.saveProduct(p);
+    }
+    
+    public void updateProduct(int id, Product p) {
+
+        if (!repo.existsById(id)) {
+            repo.saveProduct(p);
+            throw new NoSuchElementException("The product with id " + id + " does not exist.Creating a new product!");
+        }
+        p.setProductId(id);
+        repo.saveProduct(p);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteProduct(id);
+    }
+
+    /*
+    TODO:
+        get all categories
+        get Product current inventory
+        get products in a Product
+     */
 }
